1c245cf510c0d6e596b115b4e69613d841a2f343
Praveen Kumar <praveenakm0@gmail.com>
2024-08-06 00:25:09 +0530

    initial react app and Header Component

---
+++ amazon/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    //BEM
+    <div className="app">
+      <h1>Hello E-commerece</h1>
+      <Header />
+      {/* {Home} */}
+    </div>
+  );
+}
+
+export default App;
